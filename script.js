ae0dc1c901b5fc3c5b66c32b1ea7561f9d81155c
Harsh Pant <pantharsh11hp@gmail.com>
2025-11-26 21:34:39 +0530

    final

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ const VIDEO_FILE = "images/video.mp4";
 const MUSIC_FILE = "images/bg-music.mp3";
 
 const CAPTIONS = [
-  "Our veryy first memory ♡ 22nd July 2025",
-  "Our first random & fun ride together.. ♡",
-  "Third meet — someone crashed our bubble!",
-  "6th Aug — finally grabbed you ♡ hehe....",
-  "Our first food-date at KFC(6Aug) ♡ hehehe"
+  "1st date: 22nd july",
+  "2nd date: 30th july",
+  "3rd date: 04th Augest",
+  "4th date: 06th Augest",
+  "5th date: 06th Augest",
 ];
 
 // ----------------------
